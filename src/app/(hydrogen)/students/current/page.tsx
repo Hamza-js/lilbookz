af4a6f5c2b23a77742ca54bee4b43cefc383b435
Hamza-js <mhamzaawanmp1@gmail.com>
2024-01-18 21:41:38 +0500

    s

--- src/app/(hydrogen)/students/current/page.tsx
+++ src/app/(hydrogen)/students/current/page.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { routes } from '@/config/routes';
+import PageHeader from '@/app/shared/page-header';
+import ProductsGrid from '@/app/shared/explore-listing';
+import ListingFilters from '@/app/shared/explore-listing/listing-filters';
+import { metaObject } from '@/config/site.config';
+
+export const metadata = {
+  ...metaObject('Real State'),
+};
+
+const pageHeader = {
+  title: 'Students',
+  breadcrumb: [
+    {
+      name: 'Students',
+    },
+    {
+      // href: routes.searchAndFilter.realEstate,
+      name: 'Current',
+    },
+  ],
+};
 
 function StudentCurrent() {
-  return <div>student Current</div>;
+  return (
+    <div className="@container">
+      <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb} />
+      <ListingFilters className="mb-6" />
+      {/* <ProductsGrid /> */}
+    </div>
+  );
 }
 
 export default StudentCurrent;
