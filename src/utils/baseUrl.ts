f7e2a515cba606e37eb1e63d333fca0906f1f150
Hamza-js <mhamzaawanmp1@gmail.com>
2024-03-04 03:13:48 +0500

    d

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3000';
-// const baseUrl = 'https://www.lilbookz.co.uk';
+// const baseUrl = 'http://localhost:3000';
+const baseUrl = 'https://www.lilbookz.co.uk';
 
 export default baseUrl;
