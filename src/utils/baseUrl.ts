277eb4844abd31152736a31f95b28efd18bbbc34
Hamza-js <mhamzaawanmp1@gmail.com>
2024-06-04 14:24:18 +0500

    test

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,5 +1,6 @@
 // const baseUrl = 'http://localhost:3001';
-const baseUrl = 'https://www.lilbookz.co.uk';
+// const baseUrl = 'https://www.lilbookz.co.uk';
 // const baseUrl = 'https://lilbookz-temp-server.onrender.com';
+const baseUrl = 'http://18.130.154.18'
 
 export default baseUrl;
