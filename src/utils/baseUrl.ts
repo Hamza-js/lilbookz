dbd8532b0e145246ec350393e1cbdbc1ffb78971
Hamza-js <mhamzaawanmp1@gmail.com>
2024-07-01 20:46:00 +0500

    s

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,5 +1,5 @@
 // const baseUrl = 'http://localhost:3001';
-const baseUrl = 'https://www.lilbookz.co.uk';
-// const baseUrl = 'http://18.171.161.42:3000'
+// const baseUrl = 'https://www.lilbookz.co.uk';
+const baseUrl = 'http://18.171.161.42:3000'
 
 export default baseUrl;
