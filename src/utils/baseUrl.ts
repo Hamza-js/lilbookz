1694f2fbcd40467aaebda7e506b1d94bc2eb7cc5
Hamza-js <mhamzaawanmp1@gmail.com>
2024-07-09 14:30:08 +0500

    fixed all issues of company

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,5 +1,6 @@
 // const baseUrl = 'http://localhost:3001';
 // const baseUrl = 'https://www.lilbookz.co.uk';
-const baseUrl = 'http://35.177.117.14:3001'
+// const baseUrl = 'http://35.177.117.14:3001'
+const baseUrl = 'https://api.lilbeatz.com';
 
 export default baseUrl;
