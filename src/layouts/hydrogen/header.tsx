6ed1c47e1ecdd0ea7c3a32cefe30d3164ad35142
Hamza-js <mhamzaawanmp1@gmail.com>
2024-07-11 09:48:25 +0500

    s

--- src/layouts/hydrogen/header.tsx
+++ src/layouts/hydrogen/header.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 import HamburgerButton from '@/layouts/hamburger-button';
 import SearchWidget from '@/components/search/search';
 import Sidebar from '@/layouts/hydrogen/sidebar';
-import Logo from '@/components/logo';
+import Logo from '@/components/header-logo';
 import HeaderMenuRight from '@/layouts/header-menu-right';
 import StickyHeader from '@/layouts/sticky-header';
 
 export default function Header() {
   return (
-    <StickyHeader className="2xl:py-5 3xl:px-8 4xl:px-10">
+    <StickyHeader className="2xl:py-5 3xl:px-8 4xl:px-10 ">
       <div className="flex w-full max-w-2xl items-center">
         <HamburgerButton
           view={<Sidebar className="static w-full 2xl:w-full" />}
